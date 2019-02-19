d25a2e6f079072b6ee069a132742356715a2c232
Alex Zhou <alexzhou00@berkeley.edu>
2019-02-18 21:24:28 -0800

    removed most syntax errors

--- fake_publisher.py
+++ fake_publisher.py
@@ -10,28 +10,31 @@ from nav_msgs.msg import Odometry
 
 class Fake_Publisher:
 	def __init__(self):
-		self.publish_command = rospy.publisher('/goal_position', Twist, queue_size=1)
-		self.publish_odom = rospy.publisher('/odom', Odometry, queue_size=1)
+		self.publish_command = rospy.Publisher('/goal_position', Twist, queue_size=1)
+		self.publish_odom = rospy.Publisher('/odom', Odometry, queue_size=1)
 		
 		g = Twist()
 		g.linear.x = 4
 		g.linear.y = 3
 		g.angular.z = 1
+		self.g = g
 
 		o = Odometry()
 		o.pose.pose.orientation.x = 2
 		o.pose.pose.orientation.y = 3
-		o.pose.post.orientation.w = 1.2
+		o.pose.pose.orientation.w = 1.2
+		self.o = o
 
-		rate = rospy.Rate(1)
-
+		self.rate = rospy.Rate(1)
+	
+	def run(self):
 		while not rospy.is_shutdown():
-			self.publish_command.publish(g)
-			self.publish_odom(o)
-			rate.sleep()
+			self.publish_command.publish(self.g)
+			self.publish_odom.publish(self.o)
+			self.rate.sleep()
 
 if __name__ == '__main__':
 	rospy.init_node('fake_publisher')
 	fake_publisher = Fake_Publisher()
 	print 'Fake Publisher Running'
-	rospy.spin()
+	fake_publisher.run()
